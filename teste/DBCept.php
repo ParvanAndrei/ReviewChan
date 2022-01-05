e2f50478a4abb3d73081eef503b20153ed5c034c
ovi2701 <botsch_ovidiu27@yahoo.com>
2022-01-05 12:37:27 +0200

    adaugat teste DB

---
+++ teste/DBCept.php
@@ -0,0 +1,34 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('check if database connection works');
+$I->amGoingTo('register a new person and than try to log in with those dates saved in DB.');
+
+$I->amOnPage('http://localhost/ReviewChan/html/register.php');
+$I->see('Join our community, register now!');
+
+$I->amGoingTo('click on Register button to open pop-up and than enter register data');
+$I->click('.open-button');
+$I->see('ID');
+$I->see('Full Name');
+$I->see('Email');
+$I->see('Password');
+$I->see('Repeat Password');
+
+$I->amGoingTo('register a new person');
+$I->fillField('id','100');
+$I->fillField('fullname','Nick Smith');
+$I->fillField('email','nicksmith@gmail.com');
+$I->fillField('password','def');
+$I->fillField('password2','def');
+$I->click('Register','.btn');
+$I->see('New account has been created successfully, return to login page here');
+
+$I->amOnPage('http://localhost/ReviewChan/html/login.php');
+$I->see('Welcome to our community');
+
+$I->amGoingTo('try to log in with those new values. This way, I can see if DB connection works.');
+$I->fillField('txt_uname', 'nicksmith@gmail.com');
+$I->fillField('txt_pwd','def');
+$I->click('Login','.btn');
+$I->see('Welcome, Nick Smith');
+$I->see('Create Review');
